d9215e8e337db203d854eeb346abb88289208d4d
David McElroy <dmcelroy@gmail.com>
2017-11-15 23:34:02 +0000

    changed api

--- src/Services/api/footbal-data/index.js
+++ src/Services/api/footbal-data/index.js
@@ -6,6 +6,7 @@ const key = config.footballDataAPIKey;
 
 const headers = new Headers();
 headers.append('X-Auth-Token', key);
+headers.append('X-Response-Control', 'minified');
 
 const init = {
   method: 'GET',
@@ -23,24 +24,52 @@ const handleErrors = async (response) => {
 };
 
 const footballDataApi = {
-  getAll: async () => {
-    try {
-      const response = await fetch(`${url}/competitions`, init)
-        .then(handleErrors);
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      throw new Error(err);
+  competitions: {
+    getAll: async () => {
+      try {
+        const response = await fetch(`${url}/competitions`, init)
+          .then(handleErrors);
+        const data = await response.json();
+        return data;
+      }
+      catch (err) {
+        throw new Error(err);
+      }
+    },
+    getOne: async (id) => {
+      try {
+        const response = await fetch(`${url}/competitions/${id}`, init)
+          .then(handleErrors);
+        const data = await response.json();
+        return data;
+      }
+      catch (err) {
+        throw new Error(err);
+      }
     }
   },
-  getOne: async (id) => {
-    try {
-      const response = await fetch(`${url}/competitions/${id}`, init)
-        .then(handleErrors);
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      throw new Error(err);
+  fixtures: {
+    getAll: async (compId) => {
+      try {
+        const response = await fetch(`${url}/competitions/${compId}/fixtures`, init)
+          .then(handleErrors);
+        const data = await response.json();
+        return data;
+      }
+      catch (err) {
+        throw new Error(err);
+      }
+    },
+    getGameweek: async (compId, gameweek) => {
+      try {
+        const response = await fetch(`${url}/competitions/${compId}/fixtures?matchday=${gameweek}`, init)
+          .then(handleErrors);
+        const data = await response.json();
+        return data;
+      }
+      catch (err) {
+        throw new Error(err);
+      }
     }
   }
 };
