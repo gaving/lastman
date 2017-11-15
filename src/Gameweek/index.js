5d5dbc78a7a00079dda182321c92272f6d6d52de
David McElroy <dmcelroy@gmail.com>
2017-11-15 23:36:40 +0000

    use new api

--- src/Gameweek/index.js
+++ src/Gameweek/index.js
@@ -6,26 +6,57 @@ class Gameweek extends Component {
     super(props);
 
     this.state = {
-      currentMatchday: null
+      matchday: null,
+      deadline: '',
+      end: ''
     };
   }
 
   async componentDidMount() {
-    let data;
     try {
-      data = await footballDataApi.getOne(445);
+      const gameweekData = await footballDataApi.competitions.getOne(445);
+
+      this.setState({
+        matchday: gameweekData.currentMatchday
+      });
+
+      const fixtureData = await footballDataApi.fixtures.getGameweek(445, gameweekData.currentMatchday);
+
+      const dates = fixtureData.fixtures.map(f => f.date);
+      const deadline = dates.reduce((a, b) => (a < b ? a : b));
+      const end = dates.reduce((a, b) => (a > b ? a : b));
+
+      this.setState({
+        deadline: new Date(deadline),
+        end: new Date(end)
+      });
     }
     catch (err) {
-      // Handle Error
+      //console.log(err);
     }
-
-    this.setState({
-      currentMatchday: data.currentMatchday
-    });
   }
 
   render() {
-    return (<div>Current Matchday: {this.state.currentMatchday}</div>);
+    return (
+      <div>Current Matchday: {this.state.matchday}<br />
+      Deadline: {new Intl.DateTimeFormat('en-GB', {
+        weekday: 'long',
+        hour: 'numeric',
+        minute: 'numeric',
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+      }).format(this.state.deadline)}<br />
+      GameWeek Over: {new Intl.DateTimeFormat('en-GB', {
+        weekday: 'long',
+        hour: 'numeric',
+        minute: 'numeric',
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+      }).format(this.state.end)}
+      </div>
+    );
   }
 }
 
